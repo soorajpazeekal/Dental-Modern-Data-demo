26ac7d0191dc7b47a90abb39ef0f513a44e793ad
Sooraj Pazeekal <soorajpazeekal@gmail.com>
2024-01-16 11:40:13 +0530

    minor fix

--- init.sql
+++ init.sql
@@ -2,3 +2,4 @@ CREATE DATABASE IF NOT EXISTS patient_crm;
 CREATE DATABASE IF NOT EXISTS billing_and_payment;
 CREATE DATABASE IF NOT EXISTS staff_management;
 CREATE DATABASE IF NOT EXISTS inventory_and_supplies;
+
